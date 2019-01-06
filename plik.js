025728d27f53d71022d21b18a6ea9bc1fb8e5851
Fabiola Jantura <fabiolajantura@gmail.com>
2019-01-06 18:31:08 +0100

    fix the problem

--- plik.js
+++ plik.js
@@ -1,28 +1,20 @@
 function Button(text) {
 	this.text = text || 'Hello';
 }
-
 Button.prototype = {
-	create: function() {
-		this.element = document.createElement('button');
-		this.element.innerText = this.text;
-	}
-}
-
-this.element.addEventListener('click', function() {
-	alert(self.text);
-});
-
-create: function() {
+  create: function() {
 	var self = this;
 	this.element = document.createElement('button');
 	this.element.innerText = this.text;
 	this.element.addEventListener('click', function() {
-        alert(self.text);
-    document.body.appendChild(this.element);
-
+	alert(self.text);
 	});
+  document.body.appendChild(this.element);
 }
+   
+}
+  var btn1 = new Button('Hello!');
+	btn1.create();
 
-var btn1 = new Button('Hello!');
-btn1.create();
+    
+
